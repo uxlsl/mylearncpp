5b76370a2d27753746b03e2dfaaa39eadf85bd91
lin.sl <lin.sl.0001@gmail.com>
2021-01-19 15:37:36 +0800

    增加测试

---
+++ test.cpp
@@ -0,0 +1,25 @@
+#include <gtest/gtest.h>
+#include <numeric>
+#include <vector>
+// 测试集为 MyTest，测试案例为 Sum
+TEST(MyTest, Sum)
+{
+    std::vector<int> vec{1, 2, 3, 4, 5};
+    int sum = std::accumulate(vec.begin(), vec.end(), 0);
+    EXPECT_EQ(sum, 15);
+}
+
+TEST(MyTest, Abs)
+{
+    int v = std::abs(-3);
+    EXPECT_EQ(v, 3);
+}
+
+
+
+
+int main(int argc, char *argv[])
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
